3105a726ca68eeaf099e47369d8235da904b363c
Nick Addison <Nick Addison>
2017-10-23 16:03:02 -0700

    Updated dataUtility with function for finding the most frequent words in a song, this can be expanded to extract characteristic words for each genre

--- dataUtility.py
+++ dataUtility.py
@@ -13,14 +13,9 @@ class csV2Cla():
         # remove unhelpful data
         df = df.dropna()
         df.lyrics = df.lyrics.str.replace('\n', ' ')
-        df.lyrics = df.lyrics.str.replace(',', '')
-        df.lyrics = df.lyrics.str.replace('.', '')
-        df.lyrics = df.lyrics.str.replace('\[.*\]', '')
-        df.lyrics = df.lyrics.str.replace(':', '')
-        df.lyrics = df.lyrics.str.replace('(', '')
-        df.lyrics = df.lyrics.str.replace(')', '')
-        df.lyrics = df.lyrics.str.replace('!', '')
+        df.lyrics = df.lyrics.str.replace(r"[',.()!?:]|(\[.*\])", '')
         df = df[df.lyrics != "INSTRUMENTAL"]
+        df = df[df.genre != "Not Available"]
         #split the strings during processing
         #doing it all at once will take too long
         #we can limit the time taken during processing
@@ -28,9 +23,25 @@ class csV2Cla():
         self.genre = df.genre.unique()
         self.artist = df.artist.unique()
         self.year = df.year.unique()
+        self.lyrics_vector = df.lyrics.str.split(' ')
+
+    def getFreqWords(self, i, n):
+        words = np.unique(self.lyrics_vector[1], return_counts=True)
+        words = np.array(words, dtype=object).T
+        words = words[np.argsort(words[:,1])[::-1][:n]]
+        return words
 
 if __name__ == '__main__':
     cvt = csV2Cla('data/lyrics.csv')
+
     print(cvt.base.head())
     print(cvt.year)
+    print(cvt.genre)
     print(cvt.base.lyrics)
+
+    i = 1
+    print(cvt.lyrics_vector[i])
+    print('-'*10)
+    cwords = cvt.getFreqWords(i, 10)
+    print('Top 10 Most Common Words:\n{}'.format(cwords))
+    print('-'*10)
