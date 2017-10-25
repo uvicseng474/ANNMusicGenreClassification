830b3a006bbea78bfa636c3a99916a4253b86ac9
Caaraya <christina-araya@hotmail.com>
2017-10-24 19:58:10 -0700

    get most common genre counts

--- dataUtility.py
+++ dataUtility.py
@@ -13,8 +13,9 @@ class csV2Cla():
         df = pd.read_csv(fname, low_memory=False)
         # remove unhelpful data
         df = df.dropna()
-        df.lyrics = df.lyrics.str.replace('\n', ' ')
-        df.lyrics = df.lyrics.str.replace(r"[',.()!?:]|(\[.*\])", '')
+        df.lyrics = df.lyrics.str.lower().str.replace('\n', ' ')
+        df.lyrics = df.lyrics.str.replace(r"[',.()!?:\-;]|(\[.*\])", '')
+        df.lyrics = df.lyrics.str.replace(r"[^a-zA-Z ]", '')
         df = df[df.lyrics != "INSTRUMENTAL"]
         df = df[df.genre != "Not Available"]
         #split the strings during processing
@@ -24,7 +25,7 @@ class csV2Cla():
         self.genre = df.genre.unique()
         self.artist = df.artist.unique()
         self.year = df.year.unique()
-        #self.result = Counter(" ".join(df.lyrics.values.tolist()).split(" ")).items()
+        self.count_all = Counter(" ".join(df.lyrics.values.tolist()).split(" "))
         self.lyrics_vector = df.lyrics.str.split(' ')
 
     def getFreqWords(self, i, n):
@@ -35,24 +36,24 @@ class csV2Cla():
 
     def getFreqWordsByGenre(self, genre):
         return Counter(" ".join(self.base[self.base.genre == genre].lyrics.values.tolist()).split(" "))
-        
+    
 
 if __name__ == '__main__':
     cvt = csV2Cla('data/lyrics.csv')
 
-    print(cvt.base.head())
-    print(cvt.year)
-    print(cvt.genre)
-    print(cvt.base.lyrics)
+    #print(cvt.base.head())
+    #print(cvt.year)
+    #print(cvt.genre)
+    #print(cvt.base.lyrics)
 
-    i = 1
-    print(cvt.lyrics_vector[i])
-    print('-'*10)
-    cwords = cvt.getFreqWords(i, 10)
-    print('Top 10 Most Common Words:\n{}'.format(cwords))
-    print('-'*10)
+    #i = 1
+    #print(cvt.lyrics_vector[i])
+    #print('-'*10)
+    #cwords = cvt.getFreqWords(i, 10)
+    #print('Top 10 Most Common Words:\n{}'.format(cwords))
+    #print('-'*10)
 
     for y in cvt.genre:
         genredict = cvt.getFreqWordsByGenre(y)
-        print(y + " most_common ")
-        print(genredict.most_common(15))
+        print("\n" + y + " most common:")
+        print(temp.most_common(20))
